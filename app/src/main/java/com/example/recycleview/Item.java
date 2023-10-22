c76ef6b8ecf6ea767f102c8ef5cdbd980df77961
VictoriaReyes1803 <139159900+VictoriaReyes1803@users.noreply.github.com>
2023-10-22 16:23:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/Item.java
@@ -0,0 +1,57 @@
+package com.example.recycleview;
+
+public class Item {
+    String name;
+    String description;
+    int image;
+    String email;
+    String phone;
+
+    public Item(String name, String description, int image, String email, String phone) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ app/src/main/java/com/example/recycleview/MyViewHolder.java
@@ -0,0 +1,16 @@
+package com.example.recycleview;
+import android.view.View;
+
+import android.media.Image;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyViewHolder extends RecyclerView.ViewHolder   {
+
+
+
+}
